4d18ee393effcc81615c335e746aa497dd50ac28
Bjonkt <frankie.gallagher@hotmail.com>
2020-04-04 15:04:31 +0200

    Update demo3.js

--- js/demo3.js
+++ js/demo3.js
@@ -19,7 +19,7 @@ function openExtra() {
   });
   renderer.setPixelRatio(window.devicePixelRatio > 1 ? 2 : 1);
   renderer.setSize(width, height);
-  renderer.setClearColor(0xA9E7DA);
+  renderer.setClearColor(0x212121);
 
   var scene = new THREE.Scene();
 
